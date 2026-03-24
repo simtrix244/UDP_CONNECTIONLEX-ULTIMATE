2c6b64210280ae14e07ee8ddab8792e5f4ff1172
Simone <simone.orletti07@gmail.com>
2026-03-24 18:48:37 +0100

    yyy

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -1,41 +1,51 @@
 package Client;
-
 import java.io.IOException;
 import java.net.*;
+import java.util.Scanner;
 
 public class MainClient {
     public static void main(String[] args) {
         try {
-            InetAddress serverAdress = InetAddress.getLocalHost();
-            String messaggio = "ciao";
+            InetAddress serverAddress = InetAddress.getLocalHost();
             int port = 3000;
-            //scelta primitiva socket -> operazione di bind implicita
             DatagramSocket dSocket = new DatagramSocket();
-            //costruzione del pacchetto di dati
-            System.out.println("primitiva socket lato client realizzata");
-            DatagramPacket outPacket = new DatagramPacket(messaggio.getBytes(), messaggio.length(), serverAdress, port);
-            //invio dei dati
-            dSocket.send(outPacket);
-            System.out.println("Pacchetto di dati inviato al SERVER");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("CLIENT avviato. Scrivi 'stop' per chiudere.");
 
-            byte[] bufferIn = new byte[256];
-            DatagramPacket inPacket = new DatagramPacket(bufferIn, bufferIn.length);
-            dSocket.receive(inPacket);
-            System.out.println("ricezione messaggio effettuata" + inPacket);
+            while (true) {
+                System.out.print("[CLIENT - scrivi]: ");
+                String messaggioInvio = scanner.nextLine();
+                DatagramPacket outPacket = new DatagramPacket(
+                        messaggioInvio.getBytes(), messaggioInvio.length(), serverAddress, port
+                );
+                dSocket.send(outPacket);
+
+                if (messaggioInvio.equalsIgnoreCase("stop")) {
+                    System.out.println("Connessione chiusa dal client.");
+                    break;
+                }
+
+                byte[] bufferIn = new byte[256];
+                DatagramPacket inPacket = new DatagramPacket(bufferIn, bufferIn.length);
+                dSocket.receive(inPacket);
+                String messaggioRicevuto = new String(inPacket.getData(), 0, inPacket.getLength());
+                System.out.println("[SERVER]: " + messaggioRicevuto);
+
+                if (messaggioRicevuto.equalsIgnoreCase("stop")) {
+                    System.out.println("Connessione chiusa dal server.");
+                    break;
+                }
+            }
 
             dSocket.close();
-            System.out.println("comunicazione chiusa!");
+            System.out.println("Socket chiusa.");
 
         } catch (UnknownHostException e) {
             System.err.println("Errore DNS");
         } catch (SocketException e) {
             System.err.println("Errore Socket");
         } catch (IOException e) {
-            System.err.println("Errore di I/0");
-
-
+            System.err.println("Errore di I/O");
         }
-
-
     }
 }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,47 +1,51 @@
 package Server;
-
 import java.io.IOException;
 import java.net.*;
+import java.util.Scanner;
 
 public class MainServer {
-    public static void main(String[] args) throws SocketException {
-        System.out.println("SERVER");
-
+    public static void main(String[] args) {
+        System.out.println("SERVER avviato sulla porta 3000");
         try {
             DatagramSocket dSocket = new DatagramSocket(3000);
-
-
-            //ricevo il messaggio
+            Scanner scanner = new Scanner(System.in);
             byte[] bufferIn = new byte[256];
-            DatagramPacket dpi = new DatagramPacket(bufferIn, bufferIn.length);
-            dSocket.receive(dpi);
-            System.out.println("ricezione messaggio effettuata");
 
-            String messaggio = new String(dpi.getData(),dpi.getLength());
+            while (true) {
+                DatagramPacket inPacket = new DatagramPacket(bufferIn, bufferIn.length);
+                dSocket.receive(inPacket);
+                String messaggioRicevuto = new String(inPacket.getData(), 0, inPacket.getLength());
+                InetAddress clientAddress = inPacket.getAddress();
+                int clientPort = inPacket.getPort();
+                System.out.println("[CLIENT]: " + messaggioRicevuto);
 
+                if (messaggioRicevuto.equalsIgnoreCase("stop")) {
+                    System.out.println("Connessione chiusa dal client.");
+                    break;
+                }
 
-            InetAddress clientAdress = dpi.getAddress();
-            int port = dpi.getPort();
-            DatagramPacket outPacket = new DatagramPacket(messaggio.getBytes(), messaggio.length(),clientAdress,port);
-            dSocket.send(outPacket);
+                System.out.print("[SERVER - scrivi]: ");
+                String messaggioInvio = scanner.nextLine();
+                DatagramPacket outPacket = new DatagramPacket(
+                        messaggioInvio.getBytes(), messaggioInvio.length(), clientAddress, clientPort
+                );
+                dSocket.send(outPacket);
 
-            System.out.println("messaggio inviato");
+                if (messaggioInvio.equalsIgnoreCase("stop")) {
+                    System.out.println("Connessione chiusa dal server.");
+                    break;
+                }
+            }
+
+            dSocket.close();
+            System.out.println("Socket chiusa.");
 
         } catch (BindException e) {
-
-            System.err.println("Errore porta già in uso");
-        }
-        catch (SocketException e){
-            System.err.println("errore socket");
-        }
-        catch(IOException e){
+            System.err.println("Errore: porta già in uso");
+        } catch (SocketException e) {
+            System.err.println("Errore socket");
+        } catch (IOException e) {
             System.err.println("Errore di I/O");
         }
-
-
     }
-
-
-
 }
-
